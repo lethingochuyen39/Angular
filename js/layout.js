68dd686db889afb56c9db16e76af76c84269cdb6
LAPTOP-4UBQ9A0T\Admin <huyenle021039@gmail.com>
2021-11-15 10:34:05 +0700

    hw

--- js/layout.js
+++ js/layout.js
@@ -1,26 +1,25 @@
-var app=angular.module('myApp',['ngRoute']);
-app.config(function($routeProvider){
-    $routeProvider.when('/home',{
-        templateUrl:'home.html'
+var app = angular.module('myApp', ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'home.html'
     })
-    .when('/food',{
-        templateUrl:'food.html'
-    }).when('/add',{
-        templateUrl:'add.html'
-    })
-    .when('/about',{
-        templateUrl:'about.html'
-    });
+        .when('/food', {
+            templateUrl: 'food.html'
+        }).when('/add', {
+            templateUrl: 'add.html'
+        })
+        .when('/about', {
+            templateUrl: 'about.html'
+        });
 });
-
 app.controller("myCtr", function ($scope, $http) {
     $http.get("layout.json").then(function (response) {
         $scope.ds = response.data;
     });
-
-    $scope.addRow=function(){
-        $scope.ds.push({"id":$scope.id,"name":$scope.name,"price":$scope.price});
+});
+app.controller('myAbc',function($scope){
+    $scope.addRow = function () {
+        $scope.ds.push({ "id": $scope.id, "name": $scope.name, "price": $scope.price });
         alert("Add new food successfully");
     }
-   
-});
+})
